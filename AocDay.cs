bdbd30e40f9ba10cbd4c350a51b2472ac1e48685
Ernst Fluttert <e.fluttert@gmail.com>
2019-12-01 18:42:32 +0100

    Interface documentation

--- AocDay.cs
+++ AocDay.cs
@@ -1,15 +1,27 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace AdventOfCode2019
+﻿namespace AdventOfCode2019
 {
-    interface AocDay
+    /// <summary>
+    /// Default implementation of a Advent of Code challenge day
+    /// </summary>
+    internal interface AocDay
     {
+        /// <summary>
+        /// Solve part 1
+        /// </summary>
+        /// <param name="input">Input for this part</param>
+        /// <returns>string - Answer</returns>
         public string SolvePart1(string input);
 
+        /// <summary>
+        /// Solve part 2
+        /// </summary>
+        /// <param name="input">Input for this part</param>
+        /// <returns>string - Answer</returns>
         public string SolvePart2(string input);
 
+        /// <summary>
+        /// Low-tech test cases using Debug.Assert
+        /// </summary>
         public void Tests();
     }
 }
