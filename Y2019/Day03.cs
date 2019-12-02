7c99a39be3911d62569ebb181d683ab39485b623
Ernst Fluttert <e.fluttert@gmail.com>
2019-12-02 21:52:25 +0100

    start day 3

---
+++ Y2019/Day03.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AdventOfCode2019.Y2019
+{
+    public class Day03 : AocDay
+    {
+        public string SolvePart1(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string SolvePart2(string input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Tests()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
